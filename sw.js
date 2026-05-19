5976d11a6f0e5e7cedb4b59fab2f245cc9de7aa5
yoohey <53246535+yooooohey@users.noreply.github.com>
2026-05-19 20:31:54 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'guide-plus-v1';
+const CACHE_NAME = 'guide-plus-v1.2';
 const urlsToCache = [
   './',
   './index.html',
@@ -16,6 +16,17 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
   event.waitUntil(self.clients.claim());
 });
 
